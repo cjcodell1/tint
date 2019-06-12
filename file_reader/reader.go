d7b71d10feceacf045339c1a59c2946f0255ad53
Carter Codell <cjcodell1@gmail.com>
2019-06-12 11:47:53 -0400

    Added line reading functionality and DOS endings tests

--- file_reader/reader.go
+++ file_reader/reader.go
@@ -2,11 +2,15 @@ package file_reader
 
 import (
 	"os"
+    "io"
 	"io/ioutil"
+    "bufio"
+    "strings"
 )
 
 func ReadAll(path string) (string, error) {
 	f, err := os.Open(path)
+    defer f.Close()
 	if err != nil {
 		return "", err
 	}
@@ -17,4 +21,34 @@ func ReadAll(path string) (string, error) {
 	}
 
 	return string(contents), nil
+}
+
+func Lines(path string) ([]string, error) {
+
+    f, err := os.Open(path)
+    defer f.Close()
+    if err != nil {
+        return []string{}, err
+    }
+
+    lines := make([]string, 0, 1)
+    bufReaderPtr := bufio.NewReader(f)
+    for {
+        line, err := bufReaderPtr.ReadString(byte('\n'))
+        if err == io.EOF {
+            return lines, nil
+        }
+        if err != nil {
+            return lines, err
+        }
+
+        line = strings.TrimSuffix(line, "\n")
+
+        // remove the \r if file has DOS endings 
+        line = strings.TrimSuffix(line, "\r")
+
+        lines = append(lines, line)
+    }
+
+   return lines, nil
 }
