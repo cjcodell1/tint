91f894d71391fb1c78ade5c5f5521b2550a269e2
Carter Codell <cjcodell1@gmail.com>
2019-06-27 18:14:27 -0400

    typo fixed

--- tm/turing_machine_test.go
+++ tm/turing_machine_test.go
@@ -8,6 +8,15 @@ import (
     "github.com/cjcodell1/tint/tm"
 )
 
+// Run all testing functions
+func TestAll(t *testing.T) {
+    testNewTuringMachine(t)
+    testStart(t)
+    testStep(t)
+    testIsAccept(t)
+    testIsReject(t)
+}
+
 // Testing NewTuringMachine
 
 type newTMTest struct {
@@ -26,7 +35,7 @@ var newTMTests = []newTMTest{
     {[]tm.Transition{}, "same", "same", "same", false},
 }
 
-func TestNewTuringMachine(t *testing.T) {
+func testNewTuringMachine(t *testing.T) {
     for _, tc := range newTMTests {
         got, gotErr := tm.NewTuringMachine(tc.trans, tc.start, tc.accept, tc.reject)
 
@@ -68,7 +77,7 @@ var startTests = []startTest {
     {tmCaseSens, "tmCaseSens", "a A a A", tm.Config{"start", []string{"a", "A", "a", "A"}, 0}},
 }
 
-func TestStart(t *testing.T) {
+func testStart(t *testing.T) {
     for _, tc := range startTests {
         got := tc.tm.Start(tc.input)
         if toString(tc.expect) != toString(got) {
@@ -131,16 +140,16 @@ var stepTests = []stepTest{
 
     {tmWild, "tmWild", tm.Config{"start", []string{"a"}, 0}, tm.Config{"q2", []string{"x"}, 1}, true},
     {tmWild, "tmWild", tm.Config{"q2", []string{"x"}, 1}, tm.Config{"reject", []string{"x", "x"}, 2}, true},
-    {tmWild, "tmWild", tm.Config{"start", []string{"a b"}, 0}, tm.Config{"q2", []string{"x b"}, 1}, true},
-    {tmWild, "tmWild", tm.Config{"q2", []string{"x b"}, 0}, tm.Config{"accept", []string{"x x"}, 2}, true},
-    {tmWild, "tmWild", tm.Config{"start", []string{"b b"}, 0}, tm.Config{"reject", []string{"x b"}, 1}, true},
+    {tmWild, "tmWild", tm.Config{"start", []string{"a", "b"}, 0}, tm.Config{"q2", []string{"x", "b"}, 1}, true},
+    {tmWild, "tmWild", tm.Config{"q2", []string{"x", "b"}, 1}, tm.Config{"accept", []string{"x", "x"}, 2}, true},
+    {tmWild, "tmWild", tm.Config{"start", []string{"b", "b"}, 0}, tm.Config{"reject", []string{"x", "b"}, 1}, true},
 
     {tmWildFirst, "tmWildFirst", tm.Config{"start", []string{"a"}, 0}, tm.Config{"q2", []string{"x"}, 1}, true},
-    {tmWildFirst, "tmWildFirst", tm.Config{"start", []string{"b b"}, 0}, tm.Config{"q2", []string{"x b"}, 1}, true},
-    {tmWildFirst, "tmWildFirst", tm.Config{"q2", []string{"x b"}, 1}, tm.Config{"reject", []string{"x x"}, 2}, true},
+    {tmWildFirst, "tmWildFirst", tm.Config{"start", []string{"b", "b"}, 0}, tm.Config{"q2", []string{"x", "b"}, 1}, true},
+    {tmWildFirst, "tmWildFirst", tm.Config{"q2", []string{"x", "b"}, 1}, tm.Config{"reject", []string{"x", "x"}, 2}, true},
 }
 
-func TestStep(t *testing.T) {
+func testStep(t *testing.T) {
     for _, tc := range stepTests {
         got, gotErr := tc.tm.Step(tc.input)
 
@@ -180,7 +189,7 @@ var isAcceptTests = []isAcceptRejectTest{
     {tmAddMarkers, "tmAddMarkers", tm.Config{"accept", []string{}, 0}, false},
 }
 
-func TestIsAccept(t *testing.T) {
+func testIsAccept(t *testing.T) {
     for _, tc := range isAcceptTests {
         got := tc.tm.IsAccept(tc.conf)
         if got != tc.expect {
@@ -200,7 +209,7 @@ var isRejectTests = []isAcceptRejectTest{
     {tmAddMarkers, "tmAddMarkers", tm.Config{"accept", []string{}, 0}, false},
 }
 
-func TestIsReject(t *testing.T) {
+func testIsReject(t *testing.T) {
     for _, tc := range isRejectTests {
         got := tc.tm.IsReject(tc.conf)
         if got != tc.expect {
